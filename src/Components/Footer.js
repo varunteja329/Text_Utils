0ddc8321f200e9aa622f373275201e1b1ebb0163
varunteja329 <150577693+varunteja329@users.noreply.github.com>
2024-05-22 22:26:47 +0530

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,8 +5,8 @@ export default function Footer() {
     <div class="d-flex align-items-center justify-content-between" style={{position:'fixed',bottom:'0vw',width:'100vw',height:'7vw',backgroundColor:'black',color:'white',zIndex:99999}} >
         <div class="container text-center">
             
-   Developed by Gannu Abhijith Sai 
-     <a href="https://www.linkedin.com/in/gannu-abhijithsai-38342923b/"><img src={linkedin} alt="linkedin" style={{height:'3vw',width:'3vw',marginLeft:'1.5vw',marginTop:'-0.4vw'}}></img></a>
+   Developed by Nimmakuri Varun Teja 
+     <a href="https://www.linkedin.com/in/nimmakuri-varun-teja-b00aa1265?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><img src={linkedin} alt="linkedin" style={{height:'3vw',width:'3vw',marginLeft:'1.5vw',marginTop:'-0.4vw'}}></img></a>
     
      </div>
         </div>
